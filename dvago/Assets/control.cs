263ec5454c6e9c64ae52fd905d97ed3909546d0a
yoyoad <yoyoad.adi@gmail.com>
2015-03-30 09:46:00 +0300

    updates to game in engine

---
+++ dvago/Assets/control.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control : MonoBehaviour {
+	public float speed;
+	public float jump;
+	public bool grounded;
+	// Use this for initialization
+	void Start () {
+		
+		jump = jump * 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		
+		if (GetComponent<Rigidbody2D>().velocity.y == 0) {
+			grounded = true;
+		} else {
+			grounded = false;
+		}
+		
+		if(Input.GetKey(KeyCode.D)){
+			
+			GetComponent<Rigidbody2D>().velocity = new Vector2(speed,GetComponent<Rigidbody2D>().velocity.y);
+			
+		}else if(Input.GetKey(KeyCode.A)){
+			
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-speed,GetComponent<Rigidbody2D>().velocity.y);
+			
+		}else{
+			
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0,GetComponent<Rigidbody2D>().velocity.y);
+			
+		}
+		
+		if(Input.GetKeyDown(KeyCode.W) && grounded == true){
+			
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(GetComponent<Rigidbody2D>().velocity.x,jump));
+			
+		}
+
+
+
+	}
+}
